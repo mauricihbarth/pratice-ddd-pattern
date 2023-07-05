bdd152fc470dcce49e4a4f87a599855844992273
Mauricí Henrique Barth <premiersoft-maurici.barth@ailos.coop.br>
2023-07-05 14:47:16 -0300

    Fixed gitignore. Created the repository. Created the product template Installed the sequelized tool

---
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize-typescript";
+
+describe("Product repository teste", () => {
+
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            logging: false,
+            sync: { force: true }
+        });
+
+        afterEach(async () => {
+            await sequelize.close();
+        }
+    });
+});
