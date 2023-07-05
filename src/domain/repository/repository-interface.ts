e422487542b8a13fb3d79295f29c6604f7db6098
Mauricí Henrique Barth <premiersoft-maurici.barth@ailos.coop.br>
2023-07-05 20:06:47 -0300

    Created the Product repository and its unit tests.

--- src/domain/repository/repository-interface.ts
+++ src/domain/repository/repository-interface.ts
@@ -1,6 +1,6 @@
 export default interface RepositoryInterface<T>{
     create(entity:T):Promise<void>;
     update(entity:T):Promise<void>;
-    find(entity:T):Promise<T>;
+    find(entity:string):Promise<T>;
     findAll(entity:T):Promise<T[]>;
 }
